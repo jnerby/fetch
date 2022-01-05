f4aefb54af4d919fc76174b32b99f3356f37245b
Jen Nerby <nerby.jennifer@gmail.com>
2022-01-05 12:09:55 -0700

    Updated spend

--- README.txt
+++ README.txt
@@ -1,3 +1,5 @@
-source env/bin/activate
-pip3 install -r requirements.txt
-run python3 server.py to run server on local host 5000
+To view this project, run "git clone https://github.com/jnerby/fetch.git" and cd into the fetch directory in your VS Code Terminal.
+Run "pip3 install -r requirements.txt" from the command line, then run "python3 server.py." 
+Click on the address in "* Running on ..." in the terminal window to open the web service in your browse.
+
+
--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, jsonify, redirect, render_template, request
+from flask import flash, Flask, jsonify, redirect, render_template, request
 from datetime import datetime
 
 app = Flask(__name__)
@@ -41,42 +41,60 @@ def spend_points():
         # cast user submitted value to int
         to_spend = int(request.form['to-spend'])
         # initialize empty list to jsonify in response
-        resp = []
+        # resp = []
+        res = {}
 
-        # get positive transactions sorted from oldest to newest
         avail_points = []
+        total_points = 0
+
+        # sort transactions sorted from oldest to newest
         for transaction in TRANSACTIONS:
-            # get positive transactions
-            if transaction['points'] > 0:
-                # create tuples to sort by timestamp
-                tup = (transaction['timestamp'], transaction['points'], transaction['payer'])
-                avail_points.append(tup) 
+            # create tuples to sort by timestamp
+            tup = (transaction['timestamp'], transaction['points'], transaction['payer'])
+            avail_points.append(tup) 
+            total_points += transaction['points']
 
-        # sort all possible points by timestamp      
-        avail_points.sort()
+        # if user has enough points, start spending
+        if to_spend <= total_points:
 
-        for transaction in avail_points:
-            if to_spend == 0:
-                break
-            points = transaction[1]
-            # if transaction has surplus points, set to spend to 0 and subtract value of to_spend
-            if to_spend < points:
-                to_deduct = to_spend
-                to_spend = 0
-            # if to_spend exceeds transaction's points, deduct all points and subtract points from to_spend
-            if to_spend > points:
-                to_deduct = points
-                to_spend -= points 
-            # create new transaction to subtract points
-            timestamp = datetime.now().strftime("%Y-%m-%dT%H:%M:%SZ")
-            current = {'payer': transaction[2],
-                'points': -to_deduct,
-                'timestamp': timestamp}
-            # update transactions to include point deduction  
-            TRANSACTIONS.append(current)
-            resp.append({'payer': transaction[2], 'points': -to_deduct})
+            # sort all possible points by timestamp      
+            avail_points.sort()
 
-        return jsonify(resp)
+            for tup in avail_points:
+                if to_spend == 0:
+                    break
+                
+                points = tup[1]
+                payer = tup[2]
+                # if transaction has surplus points, set to spend to 0 and subtract value of to_spend
+                if to_spend <= points:
+                    to_deduct = to_spend
+                    to_spend = 0
+                # if to_spend exceeds transaction's points, deduct all points and subtract points from to_spend
+                if to_spend > points:
+                    to_deduct = points
+                    to_spend -= points 
+
+                # create new transaction to subtract points
+                timestamp = datetime.now().strftime("%Y-%m-%dT%H:%M:%SZ")
+                current = {'payer': tup[2],
+                    'points': -to_deduct,
+                    'timestamp': timestamp}
+                # update transactions to include point deduction  
+                TRANSACTIONS.append(current)
+
+                if payer in res:
+                    res[payer] -= to_deduct
+                else:
+                    res[payer] = -to_deduct
+
+            return jsonify(res)
+            # return jsonify(resp)
+
+        # if user does not have enough points, notify
+        else:
+            flash(f"Insufficient funds. Current point balance is {total_points}.")
+            return redirect('/')
 
     # if method is GET, redirect to base
     return redirect('/')
